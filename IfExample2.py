0615709c51f6042b790ec5a7037acce5d4520771
Bricknermon <94518180+bricknermon@users.noreply.github.com>
2023-01-08 21:41:18 -0600

    Add files via upload

---
+++ IfExample2.py
@@ -0,0 +1,10 @@
+# Practice with if, Else, and elif statments
+# Brian F.
+
+print('Please type Alice')
+name = input()
+if name == 'Alice':
+    print('Hi, Alice')
+else:   #indentation is very important! 
+    print('You did not type Alice : (')
+print('Done')

57ee738a9ec62fcefda419992cece9ed98565ac3
Bricknermon <94518180+bricknermon@users.noreply.github.com>
2023-01-08 20:59:10 -0600

    Add files via upload
    Program to practice gathering user input, functions to find lengths of data types, and print statements.

---
+++ Hello1.py
@@ -0,0 +1,15 @@
+# This program says hello and asks for my name
+# By Brian F.
+
+print('Hello!')
+print('What is your name?') # ask for their name
+name = input() # Store user input into name
+print('Nice to meet you ' + name)
+print('The length of your name is:')
+print(len(name))
+print('What is your age?') #ask for their age
+age = input() # Store user input into age
+print('You will be ' + str(int(age) + 1) + ' in a year!')
+
+# I want to improve this program by adding checks to user inputs
+# ensuring proper inputs. Also learning long commenting : )
